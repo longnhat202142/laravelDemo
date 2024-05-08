aa29c1d4eb6bdca9bf60f2d96f0ca6b4f47254a9
thnminh <20t1020460@husc.edu.vn>
2024-05-08 08:51:43 +0700

    Nhánh first Minh

---
+++ resources/views/components/component/footer.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href={{ asset('assets/clients/css/bootstrap.min.css') }} rel="stylesheet">
+    <link href={{ asset('assets/clients/css/style.css') }} rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Document</title>
+</head>
+<body>
+    
+    <footer class="text-white text-center text-lg-start" style="background-color: #143980">
+        <!-- Grid container -->
+        <div class="container p-4">
+          <!--Grid row-->
+          <div class="row mt-4">
+            <!--Grid column-->
+            <div class="col-lg-4 col-md-12 mb-4 mb-md-0">
+              <h5 class="text-uppercase mb-4">Trường Đại học Khoa học, ĐH Huế</h5>
+      
+              <p>
+                <i class="fa-solid fa-location-dot"></i> 77 Nguyễn Huệ, TP.Huế
+              </p>
+      
+              <p>
+                <i class="fa-solid fa-phone"></i><a href="tel:02343823290" class="text-decoration-none" style="color: white"> (0234) 3823290</a>
+              </p>
+      
+              <p>
+                <i class="fa-solid fa-envelope"></i><a href="mailto:khcndhkh@hueuni.edu.vn" class="text-decoration-none" style="color: white"> khcndhkh@hueuni.edu.vn</a>
+              </p>
+      
+              <p>
+                <i class="fa-brands fa-facebook"></i><a href="https://www.facebook.com/husc.edu.vn" class="text-decoration-none" style="color: white"> https://www.facebook.com/husc.edu.vn</a>
+              </p>
+      
+            </div>
+            <!--Grid column-->
+      
+            <!--Grid column-->
+            <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+              <h5 class="text-uppercase mb-4 pb-1">Thông tin về Trường</h5>
+                  <p ><i class="fa fa-caret-right"></i><a href="#TongQuang" class="text-decoration-none" style="color: white"> Tổng quan</a></p>
+                   <p><i class="fa fa-caret-right"></i><a href="#CCTC" class="text-decoration-none" style="color: white"> Cơ cấu tổ chức</a></p>
+                  <p><i class="fa fa-caret-right"></i><a href="#hotline" class="text-decoration-none" style="color: white"> Đường dây nóng</a></p>
+                  <p><i class="fa fa-caret-right"></i><a href="#CLGD" class="text-decoration-none" style="color: white"> Công khai chất lượng giáo dục</a></p>
+               
+            </div>
+            <!--Grid column-->
+      
+            <!--Grid column-->
+            <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+              <h5 class="text-uppercase mb-4">Liên kết</h5>
+                  <p ><i class="fa fa-caret-right"></i><a href="#" class="text-decoration-none" style="color: white"> Thông tin tuyển sinh</a></p>
+                 <p><i class="fa fa-caret-right"></i><a href="#" class="text-decoration-none" style="color: white"> Trang thông tin đào tạo tín chỉ</a></p>
+                <p><i class="fa fa-caret-right"></i><a href="#" class="text-decoration-none" style="color: white"> Thanh toán học phí trực tuyến</a></p>
+           </div>
+            <!--Grid column-->
+          </div>
+          <!--Grid row-->
+        </div>
+        <!-- Grid container -->
+      
+        <!-- Copyright -->
+        
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+          Copyright © HUSC
+        </div>
+        <!-- Copyright -->
+      </footer>
+</body>
+<script href={{asset('assets/clients/js/bootstrap.min.css')}}></script>
+</html>
