84182ba036a72c2c47ad421a0c98b7b0c7e316cf
Huu Nhat <nhatnguyen2204n@gmail.com>
2024-07-01 09:49:29 +0700

    01-07

--- routes/web.php
+++ routes/web.php
@@ -81,7 +81,7 @@ Route::get('error', function () {
       Route::get('/login',[LoginController::class,'showLoginForm'])->name('login');
       Route::post('/login',[LoginController::class,'login']);
       Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-      //Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+      Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
       Route::post('/register', [RegisterController::class, 'create'])->name('register');
       Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
     //đổi mật khẩu 
